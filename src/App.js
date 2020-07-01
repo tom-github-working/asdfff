d1cb80eb51177576e5c3c2acafc2d84fb339fca0
Tom Woranai <delete_x1@hotmail.com>
2020-07-01 21:57:44 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+    <div>
+     <Header />
+     <Footer company ="IO" email = "IO@hotmail.com" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Headers = () => {
+    return <h1>HEADER</h1>
+}
+
+export default Headers;
